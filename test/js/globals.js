bbb179c8342a289172ad485c6e5e0766be015f99
Роман Агафонов <reagafonov@yandex.ru>
2025-08-25 11:11:13 +0300

    События

--- test/js/globals.js
+++ test/js/globals.js
@@ -1,6 +1,31 @@
 var ShowRemovedOnly = false;
-var state = {
-    selectedFileId: null,
-    selectedTaskId: null,
-    selectedListId: null,
+var state = null;
+function HandlerIdAdapter(handler) {
+    this.handler = handler,
+    this.adapter = function(event) {
+        handler(event.detail.id);
+    }
 }
+
+$(function() {
+    ShowRemovedOnly = false;
+    state = {
+        selectedFileId: null,
+        selectedTaskId: null,
+        selectedListId: null,
+        changedFileId: function (id)
+        {
+            var event = new CustomEvent('taskfilefilechangedid',{
+                detail: {
+                    id:id
+                },
+            });
+            document.dispatchEvent(event);
+        },
+        subscribe_changedFileId:   function (handler)
+        {
+            var adapter = new HandlerIdAdapter(handler);
+            document.addEventListener('taskfilefilechangedid', adapter.adapter);
+        }
+    }
+})
